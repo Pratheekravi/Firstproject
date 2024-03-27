8eb6b2d0da1919c6f8be34c9f5513bc6fe45b221
Pratheekravi <164884492+Pratheekravi@users.noreply.github.com>
2024-03-27 18:03:59 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,17 @@
+class Calculator {
+public static void main(String [] args){ 
+	System.out.println("hello world");
+	int x = 5;
+	int y = 10;
+	int z = x - y;
+	System.out.println("the output of x - y is " + z);
+
+}
+
+
+
+
+
+}
+
+	
